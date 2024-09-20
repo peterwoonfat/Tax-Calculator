665da587a471c988d65fe687a52277eae7a5ec89
Peter Woon-Fat <peterwoonfat@yahoo.ca>
2024-09-20 00:30:34 -0400

    Added meta components to specify the document metadata

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const meta = {
+    title: 'Some Meta Title',
+    description: 'I am a description, and I can create multiple tags',
+    canonical: 'http://example.com/path/to/page',
+    meta: {
+        charset: 'utf-8',
+        name: {
+            keywords: 'react,meta,document,html,tags'
+        }
+    }
+  }
   return (
     <div className="App">
+      <meta name="keywords" content="HTML, CSS, JavaScript, React, Node.js"/>
+      <meta name="author" content="Peter Woon-Fat"/>
+      <meta name="description" content="Web app used to calculate tax liabilities"/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +33,9 @@ function App() {
         </a>
       </header>
     </div>
+
+    // single-page web app to calculate tax liabilities
+
   );
 }
 
