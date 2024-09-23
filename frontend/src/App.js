1570c307f0907d7889618d2297d10f61c5d24bc2
Peter Woon-Fat <peterwoonfat@yahoo.ca>
2024-09-22 22:37:36 -0400

    Fixed meta tags - moved from App.js to index.html as react can not change meta tags

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
+// single-page web app to calculate tax liabilities
 function App() {
-  useEffect(() => {
-    document.title = 'Tax Liability Calculator';
-  }, []);
+  // useEffect(() => {
+  //   document.title = 'Tax Liability Calculator';
+  // }, []);
 
   return (
     <div className="App">
-      {/* set document metadata */}
-      <meta name="keywords" content="HTML, CSS, JavaScript, React, Node.js"/>
-      <meta name="author" content="Peter Woon-Fat"/>
-      <meta name="description" content="Web app used to calculate tax liabilities"/>
-      <meta charset="utf-8"/>
-      <meta name="viewport" content="width=device-width, initial-scale=1"/>
-
       {/* example content */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -33,16 +30,43 @@ function App() {
           Learn React
         </a>
       </header>
+      {/* input forms for tax calculation parameters */}
+      <div>
+        {/* <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form> */}
 
-      {/* stacked input forms for tax calculation parameters */}
-      <Stack gap={3}>
-        <div className="p-2">First item</div>
-        <div className="p-2">Second item</div>
-        <div className="p-2">Third item</div>
-      </Stack>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Label>form 1</Form.Label>
+            </Col>
+            <Col>
+              <Form.Control/>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Label>form 2</Form.Label>
+            </Col>
+            <Col>
+              <Form.Control/>
+            </Col>
+          </Row>
+        </Form>
+
+      </div>
+
+
+      {/* can use a range form for tax brackets */}
     </div>
-
-    // single-page web app to calculate tax liabilities
 
   );
 }
