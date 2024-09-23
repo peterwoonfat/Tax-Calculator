9d84e47479ef33cfe729e1609e6420cb211d95b5
Peter Woon-Fat <peterwoonfat@yahoo.ca>
2024-09-23 14:12:22 -0400

    Cleaned up css formatting and colouring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container'
 
 
 // single-page web app to calculate tax liabilities
@@ -17,36 +18,27 @@ function App() {
         <h1>Peter's Tax Liability Calculator</h1>
       </header>
       {/* input forms for tax calculation parameters */}
-      <div>
-        {/* <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Example textarea</Form.Label>
-            <Form.Control as="textarea" rows={3} />
-          </Form.Group>
-        </Form> */}
-
-        <Form>
-          <Row>
-            <Col>
-              <Form.Label>form 1</Form.Label>
-            </Col>
-            <Col>
-              <Form.Control/>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Label>form 2</Form.Label>
-            </Col>
-            <Col>
-              <Form.Control/>
-            </Col>
-          </Row>
-        </Form>
+      <div className='App-forms'>
+        <Container>
+          <Form>
+            <Row>
+              <Col>
+                <Form.Label>form 1</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control/>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Form.Label>form 2</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control/>
+              </Col>
+            </Row>
+          </Form>
+        </Container>
 
       </div>
 
