110b12ca8d8e09c787d1b6320780b44bbe18556d
Peter Woon-Fat <peterwoonfat@yahoo.ca>
2024-09-26 15:14:30 -0400

    Added all income forms and adjusted styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import react-bootstrap components
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container'
+import InputGroup from 'react-bootstrap/InputGroup'
 
 
 // single-page web app to calculate tax liabilities
@@ -20,11 +22,58 @@ function App() {
         <Container>
           <Form>
             <Form.Group as={Row} className='mb-3' controlId='formEmploymentIncome'>
-              <Form.Label column sm={2}>
+              <Form.Label column sm={2} className='form-label'>
                 Employment Income
               </Form.Label>
               <Col sm={2}>
-                <Form.Control type='number'/>
+                <InputGroup className='mb-2'>
+                  <InputGroup.Text className='no-bg'>$</InputGroup.Text>
+                  <Form.Control type='number' min='0'/>
+                </InputGroup>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className='mb-3' controlId='formSelfEmploymentIncome'>
+              <Form.Label column sm={2} className='form-label'>
+                Self Employment Income
+              </Form.Label>
+              <Col sm={2}>
+                <InputGroup className='mb-2'>
+                  <InputGroup.Text className='no-bg'>$</InputGroup.Text>
+                  <Form.Control type='number' min='0'/>
+                </InputGroup>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className='mb-3' controlId='formInvestmentIncome'>
+              <Form.Label column sm={2} className='form-label'>
+                Investment Income
+              </Form.Label>
+              <Col sm={2}>
+                <InputGroup className='mb-2'>
+                  <InputGroup.Text className='no-bg'>$</InputGroup.Text>
+                  <Form.Control type='number' min='0'/>
+                </InputGroup>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className='mb-3' controlId='formCapitalGainLoss'>
+              <Form.Label column sm={2} className='form-label'>
+                Capital Gains/Losses
+              </Form.Label>
+              <Col sm={2}>
+                <InputGroup className='mb-2'>
+                  <InputGroup.Text className='no-bg'>$</InputGroup.Text>
+                  <Form.Control type='number' min='0'/>
+                </InputGroup>
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} className='mb-3' controlId='formOtherIncome'>
+              <Form.Label column sm={2} className='form-label'>
+                Other Income
+              </Form.Label>
+              <Col sm={2}>
+                <InputGroup className='mb-2'>
+                  <InputGroup.Text className='no-bg'>$</InputGroup.Text>
+                  <Form.Control type='number' min='0'/>
+                </InputGroup>
               </Col>
             </Form.Group>
           </Form>
